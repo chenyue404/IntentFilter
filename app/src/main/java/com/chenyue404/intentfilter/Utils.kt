01838e127f66bbc6ad6bab6ebdf328dd057a7e64
chenyue404 <601450225@qq.com>
2021-04-07 16:37:16 +0800

    Intent str util

--- app/src/main/java/com/chenyue404/intentfilter/Utils.kt
+++ app/src/main/java/com/chenyue404/intentfilter/Utils.kt
@@ -1,8 +1,11 @@
 package com.chenyue404.intentfilter
 
 import android.content.Context
+import android.content.Intent
 import android.view.View
 import com.google.gson.Gson
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
 import com.google.gson.reflect.TypeToken
 import java.text.SimpleDateFormat
 import java.util.*
@@ -27,4 +30,38 @@ fun getResourceIdByName(context: Context, name: String): Int {
 
 fun View.visible(visibility: Boolean) {
     this.visibility = if (visibility) View.VISIBLE else View.GONE
+}
+
+fun Intent.transToStr(): String {
+    val jsonObject = JsonObject().apply {
+        addProperty("to", component?.className ?: "null")
+        addProperty("action", action)
+        addProperty("clipData", clipData.toString())
+        addProperty("flags", flags)
+        addProperty("dataString", dataString)
+        addProperty("type", type)
+        addProperty("componentName", component.toString())
+        addProperty("scheme", scheme)
+        addProperty("package", `package`)
+        addProperty("categories", categories?.joinToString() ?: "null")
+        extras?.let { bundle ->
+            if (!bundle.isEmpty) {
+                val jsonArray = JsonArray()
+                bundle.keySet().forEach {
+                    val value = bundle.get(it)
+                    value?.run {
+                        jsonArray.add(JsonObject().apply {
+                            addProperty("key", it)
+                            addProperty("value", value.toString())
+                            addProperty("class", value.javaClass.name)
+                        })
+                    }
+
+                }
+                add("intentExtras", jsonArray)
+            }
+        }
+    }
+
+    return jsonObject.toString()
 }
