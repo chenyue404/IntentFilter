a74a7d7ce96a721aa089fb03f673c19151c74526
chenyue404 <601450225@qq.com>
2025-10-11 13:55:21 +0800

    优化日志页的性能

--- app/src/main/java/com/chenyue404/intentfilter/entity/LogEntity.kt
+++ app/src/main/java/com/chenyue404/intentfilter/entity/LogEntity.kt
@@ -1,5 +1,9 @@
 package com.chenyue404.intentfilter.entity
 
+import com.chenyue404.intentfilter.App
+import com.chenyue404.intentfilter.queryActivityInfo
+import com.chenyue404.intentfilter.queryAppInfo
+
 data class LogEntity(
     val time: Long,
     val from: String = "",
@@ -8,4 +12,45 @@ data class LogEntity(
     val dataString: String = "",
     val activities: String = "",
     val blockIndexes: String = ""
-)
+) {
+    lateinit var fromInfoList: List<Pair<String, BasicInfo>>
+    lateinit var activityInfoList: List<Pair<String, BasicInfo>>
+    lateinit var blockList: List<String>
+
+    fun processData() {
+        val fromList = from.split(App.SPLIT_LETTER)
+        fromInfoList = buildInfoList(fromList)
+
+        val activityList = activities.split(App.SPLIT_LETTER)
+        activityInfoList = buildInfoList(activityList)
+
+        val indexList = blockIndexes.split(App.SPLIT_LETTER)
+        blockList = indexList.mapNotNull {
+            it.toIntOrNull()?.let { index -> activityList.getOrNull(index) }
+        }
+    }
+
+    private fun buildInfoList(list: List<String>): List<Pair<String, BasicInfo>> {
+        val newList: MutableList<Pair<String, BasicInfo>> = mutableListOf()
+        val lastPkg = mutableListOf<String>()
+        list.forEach {
+            val isActivity = it.contains("/")
+            if (isActivity) {
+                val pkg = it.split("/").first()
+                if (lastPkg.firstOrNull() == pkg) {
+                    if (lastPkg.size == 1) {
+                        newList.add(newList.size - 1, pkg to queryAppInfo(pkg))
+                    }
+                } else {
+                    lastPkg.clear()
+                }
+                lastPkg.add(pkg)
+                newList.add(it to queryActivityInfo(it))
+            } else {
+                newList.add(it to queryAppInfo(it))
+            }
+        }
+        return newList
+    }
+
+}
--- app/src/main/java/com/chenyue404/intentfilter/ui/LogFragment.kt
+++ app/src/main/java/com/chenyue404/intentfilter/ui/LogFragment.kt
@@ -14,6 +14,9 @@ import android.widget.TextView
 import androidx.core.content.edit
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.chenyue404.intentfilter.App
 import com.chenyue404.intentfilter.LogReceiver
@@ -22,9 +25,10 @@ import com.chenyue404.intentfilter.R
 import com.chenyue404.intentfilter.dp2Px
 import com.chenyue404.intentfilter.entity.BasicInfo
 import com.chenyue404.intentfilter.entity.LogEntity
-import com.chenyue404.intentfilter.queryActivityInfo
-import com.chenyue404.intentfilter.queryAppInfo
 import com.chenyue404.intentfilter.timeToStr
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LogFragment : Fragment() {
     private val TAG = "intentfilter-hook-"
@@ -57,6 +61,7 @@ class LogFragment : Fragment() {
                 )
             )
             adapter = listAdapter
+            setItemViewCacheSize(5)
         }
 
         var sendBroadcast = sp?.getBoolean(App.KEY_SEND_BROADCAST, false) ?: false
@@ -74,11 +79,16 @@ class LogFragment : Fragment() {
             listAdapter.clear()
         }
 
-        logReceiver = LogReceiver {
-            dataList.add(it)
-            listAdapter.notifyItemInserted(dataList.lastIndex)
-            if (!rvList.canScrollVertically(1)) {
-                rvList.scrollToPosition(dataList.lastIndex)
+        logReceiver = LogReceiver { logEntityFromBroadcast ->
+            lifecycleScope.launch(Dispatchers.IO) {
+                logEntityFromBroadcast.processData()
+                withContext(Dispatchers.Main) {
+                    dataList.add(logEntityFromBroadcast)
+                    listAdapter.notifyItemInserted(dataList.lastIndex)
+                    if (!rvList.canScrollVertically(1)) {
+                        rvList.scrollToPosition(dataList.lastIndex)
+                    }
+                }
             }
         }
         requireActivity().registerReceiver(logReceiver, IntentFilter().apply {
@@ -101,61 +111,12 @@ class LogFragment : Fragment() {
     private class LogListAdapter(val dataList: ArrayList<LogEntity>) :
         RecyclerView.Adapter<LogListAdapter.ViewHolder>() {
         private var unfoldIndexList = mutableListOf<Int>()
-
+        private val viewPool = RecyclerView.RecycledViewPool()
         fun clear() {
             unfoldIndexList.clear()
             val size = dataList.size
             dataList.clear()
             notifyItemRangeRemoved(0, size)
-        }
-
-        private fun updateFold(holder: ViewHolder, position: Int, fromClick: Boolean = false) {
-            val logEntity = dataList[position]
-            val fromList = logEntity.from.split(App.SPLIT_LETTER)
-            val activityList = logEntity.activities.split(App.SPLIT_LETTER)
-            val indexList = logEntity.blockIndexes.split(App.SPLIT_LETTER)
-            if (fromClick) {
-                with(unfoldIndexList) {
-                    if (contains(position)) remove(position)
-                    else add(position)
-                }
-            }
-            val unfold = unfoldIndexList.contains(position)
-            with(holder) {
-                btMore.rotation = if (unfold) 180f else 0f
-                if (!unfold && fromList.size > 1) {
-                    tvFrom.text = "${fromList.size} apps"
-                    tvFrom.isVisible = true
-                    rvFrom.isVisible = false
-                } else {
-                    tvFrom.isVisible = false
-                    rvFrom.apply {
-                        isVisible = true
-                        layoutManager = NonScrollableLinearLayoutManager(itemView.context)
-                        adapter = InfoAdapter().apply {
-                            setData(buildInfoList(fromList))
-                        }
-                    }
-                }
-
-//                if (!unfold && activityList.size > 1) {
-//                    tvActivities.text = "${activityList.size} activities"
-//                    tvActivities.isVisible = true
-//                    rvActivity.isVisible = false
-//                } else {
-//                    tvActivities.isVisible = false
-//                    rvActivity.apply {
-//                        isVisible = true
-//                        layoutManager = NonScrollableLinearLayoutManager(itemView.context)
-//                        adapter = InfoAdapter().apply {
-//                            val blockList = indexList.mapNotNull {
-//                                it.toIntOrNull()?.let { index -> activityList.getOrNull(index) }
-//                            }
-//                            setData(buildInfoList(activityList), blockList)
-//                        }
-//                    }
-//                }
-            }
         }
 
         private class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -170,68 +131,116 @@ class LogFragment : Fragment() {
             val btMore: ImageButton = itemView.findViewById(R.id.btMore)
         }
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_log, parent, false)
-        )
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val holder = ViewHolder(
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_log, parent, false)
+            )
+            holder.rvActivity.apply {
+                setRecycledViewPool(viewPool)
+                layoutManager = NonScrollableLinearLayoutManager(context)
+                adapter = InfoAdapter()
+                (layoutManager as NonScrollableLinearLayoutManager).initialPrefetchItemCount = 10
+            }
+            holder.rvFrom.apply {
+                setRecycledViewPool(viewPool)
+                layoutManager = NonScrollableLinearLayoutManager(context)
+                adapter = InfoAdapter()
+                (layoutManager as NonScrollableLinearLayoutManager).initialPrefetchItemCount = 5
+            }
+            return holder
+        }
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
             val logEntity = dataList[position]
-            val fromList = logEntity.from.split(App.SPLIT_LETTER)
-            val activityList = logEntity.activities.split(App.SPLIT_LETTER)
-            val indexList = logEntity.blockIndexes.split(App.SPLIT_LETTER)
-            val unfold = unfoldIndexList.contains(position)
             with(holder) {
-                btMore.setOnClickListener {
-                    updateFold(holder, position, true)
-                }
-                btMore.isVisible = fromList.size > 1
                 tvTime.text = logEntity.time.timeToStr()
                 tvAction.text = logEntity.action
                 tvType.text = logEntity.type
                 tvDataString.text = logEntity.dataString
 
-                tvActivities.isVisible = false
-                rvActivity.apply {
-                    isVisible = true
-                    layoutManager = NonScrollableLinearLayoutManager(itemView.context)
-                    adapter = InfoAdapter().apply {
-                        val blockList = indexList.mapNotNull {
-                            it.toIntOrNull()?.let { index -> activityList.getOrNull(index) }
+                (rvActivity.adapter as InfoAdapter).setData(
+                    logEntity.activityInfoList,
+                    logEntity.blockList
+                )
+
+                btMore.setOnClickListener {
+                    val currentPosition = adapterPosition
+                    if (currentPosition != RecyclerView.NO_POSITION) {
+                        if (unfoldIndexList.contains(currentPosition)) {
+                            unfoldIndexList.remove(currentPosition)
+                        } else {
+                            unfoldIndexList.add(currentPosition)
                         }
-                        setData(buildInfoList(activityList), blockList)
+                        notifyItemChanged(currentPosition, "PAYLOAD_FOLD_STATE_CHANGED")
                     }
                 }
+
+                updateFoldStateUI(holder, logEntity, unfoldIndexList.contains(position))
             }
-            updateFold(holder, position)
+        }
+
+        override fun onBindViewHolder(
+            holder: ViewHolder,
+            position: Int,
+            payloads: MutableList<Any>
+        ) {
+            if (payloads.isEmpty()) {
+                super.onBindViewHolder(holder, position, payloads)
+            } else {
+                if (payloads.contains("PAYLOAD_FOLD_STATE_CHANGED")) {
+                    val logEntity = dataList[position]
+                    updateFoldStateUI(holder, logEntity, unfoldIndexList.contains(position))
+                }
+            }
+        }
+
+        private fun updateFoldStateUI(
+            holder: ViewHolder,
+            logEntity: LogEntity,
+            isUnfolded: Boolean
+        ) {
+            with(holder) {
+                btMore.isVisible = logEntity.fromInfoList.size > 1
+                btMore.rotation = if (isUnfolded) 180f else 0f
+
+                val fromListSize = logEntity.fromInfoList.size
+                if (!isUnfolded && fromListSize > 1) {
+                    tvFrom.text = "$fromListSize apps"
+                    tvFrom.isVisible = true
+                    rvFrom.isVisible = false
+                } else {
+                    tvFrom.isVisible = false
+                    rvFrom.isVisible = true
+                    (rvFrom.adapter as InfoAdapter).setData(logEntity.fromInfoList)
+                }
+            }
         }
 
         override fun getItemCount() = dataList.size
-
-        private fun buildInfoList(list: List<String>): List<Pair<String, BasicInfo>> {
-            val newList: MutableList<Pair<String, BasicInfo>> = mutableListOf()
-            val lastPkg = mutableListOf<String>()
-            list.forEach {
-                val isActivity = it.contains("/")
-                if (isActivity) {
-                    val pkg = it.split("/").first()
-                    if (lastPkg.firstOrNull() == pkg) {
-                        if (lastPkg.size == 1) {
-                            newList.add(newList.size - 1, pkg to queryAppInfo(pkg))
-                        }
-                    } else {
-                        lastPkg.clear()
-                    }
-                    lastPkg.add(pkg)
-                    newList.add(it to queryActivityInfo(it))
-                } else {
-                    newList.add(it to queryAppInfo(it))
-                }
-            }
-            return newList
-        }
     }
 
     private class InfoAdapter() : RecyclerView.Adapter<InfoAdapter.VH>() {
+        private val diffCallback = object : DiffUtil.ItemCallback<Pair<String, BasicInfo>>() {
+            override fun areItemsTheSame(
+                oldItem: Pair<String, BasicInfo>,
+                newItem: Pair<String, BasicInfo>
+            ): Boolean {
+                return oldItem.first == newItem.first
+            }
+
+            override fun areContentsTheSame(
+                oldItem: Pair<String, BasicInfo>,
+                newItem: Pair<String, BasicInfo>
+            ): Boolean {
+                val oldIsBlocked = blockList.contains(oldItem.first)
+                val newIsBlocked = blockList.contains(newItem.first)
+                return oldItem.second == newItem.second && oldIsBlocked == newIsBlocked
+            }
+        }
+        private val differ = AsyncListDiffer(this, diffCallback)
+        private var blockList: List<String> = emptyList()
+
         private class VH(itemView: View) : RecyclerView.ViewHolder(itemView) {
             val ivIcon: ImageView = itemView.findViewById(R.id.ivIcon)
             val tvName: TextView = itemView.findViewById(R.id.tvName)
@@ -239,13 +248,12 @@ class LogFragment : Fragment() {
             val sp0: Space = itemView.findViewById(R.id.sp0)
         }
 
-        private var dataList: List<Pair<String, BasicInfo>> = listOf()
-        private var blockList: List<String> = listOf()
-
-        fun setData(list: List<Pair<String, BasicInfo>>, blockList: List<String> = emptyList()) {
-            dataList = list.toList()
-            this.blockList = blockList.toList()
-            notifyItemRangeChanged(0, dataList.size)
+        fun setData(
+            newList: List<Pair<String, BasicInfo>>,
+            newBlockList: List<String> = emptyList()
+        ) {
+            blockList = newBlockList
+            differ.submitList(newList)
         }
 
         override fun onCreateViewHolder(
@@ -257,7 +265,7 @@ class LogFragment : Fragment() {
         )
 
         override fun onBindViewHolder(holder: VH, position: Int) {
-            val pair = dataList[position]
+            val pair = differ.currentList[position]
             val pkg = pair.first
             val info = pair.second
             val isBlocked = blockList.contains(pkg)
@@ -271,7 +279,11 @@ class LogFragment : Fragment() {
                 tvPkg.setTextColor(textColor)
                 tvName.text = info.label
                 val split = pkg.split("/")
-                val lastAppPkg = dataList.getOrNull(position - 1)?.first?.split("/")?.first()
+                val lastAppPkg = if (position > 0) {
+                    differ.currentList.getOrNull(position - 1)?.first?.split("/")?.first()
+                } else {
+                    null
+                }
                 val showAsActivity = split.size > 1 && lastAppPkg == split.first()
                 tvPkg.text =
                     if (showAsActivity) split.last()
@@ -280,6 +292,6 @@ class LogFragment : Fragment() {
             }
         }
 
-        override fun getItemCount() = dataList.size
+        override fun getItemCount() = differ.currentList.size
     }
 }
